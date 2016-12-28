2bf314b4830a23433bae02288595a03f42b3e0a5
callmewhy <whywanghai@gmail.com>
2016-12-28 15:36:11 +0800

    add bin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,10 @@
-{
+module.exports = {
   root: true,
   extends: 'airbnb-base',
   rules: {
     'semi': [2, 'never'],
     'quotes': [2, 'single'],
-    'no-console': 0
+    'no-console': 0,
+    'import/prefer-default-export': 0,
   }
 }
