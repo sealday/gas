66ed8ea7f1b87d66283176f74910034e673f2c8d
callmewhy <whywanghai@gmail.com>
2016-12-31 15:14:07 +0800

    git branch

---
+++ src/bin/gan-branch.js
@@ -0,0 +1,57 @@
+const inquirer = require('inquirer')
+const exec = require('child_process').exec
+const execSync = require('child_process').execSync
+const log = require('./log')
+
+function getBranches() {
+  return new Promise((resolve, reject) => {
+    exec('git branch', (error, stdout) => {
+      if (error) {
+        reject(error)
+      } else {
+        const branches = stdout.split('\n')
+                               .map(name => name.trim())
+                               .filter(name => name.length > 0)
+        resolve(branches)
+      }
+    })
+  })
+}
+
+function showBranches(branches) {
+  let currentIndex = -1
+  branches.forEach((name, i) => {
+    if (name.match(/^\* /) !== null) {
+      currentIndex = i
+    }
+  })
+
+  return inquirer.prompt([{
+    type: 'list',
+    name: 'name',
+    message: 'What branch do you want to checkout?',
+    choices: branches,
+    default: currentIndex,
+  }]).then((answers) => {
+    return answers.name
+  })
+}
+
+function switchBranch(branch) {
+  if (branch.match(/^\* /) !== null) {
+    return
+  }
+  execSync(`git checkout ${branch}`, { stdio: 'inherit' })
+}
+
+const params = process.argv.slice(2)
+if (params.length === 0) {
+  getBranches()
+    .then(showBranches)
+    .then(switchBranch)
+    .catch((error) => {
+      log.red(error)
+    })
+} else {
+  execSync(`git branch ${params.join(' ')}`, { stdio: 'inherit' })
+}
